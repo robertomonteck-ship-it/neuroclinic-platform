3b93d6ba5344b73e6b51133af553cbbb666bcd9a
Roberto Montechiari Werneck <roberto.monteck@gmail.com>
2026-04-06 18:40:42 -0300

    Added security module with password hashing and JWT

---
+++ apps/api/app/core/security.py
@@ -0,0 +1,53 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+
+from app.core.settings import settings
+
+# Configuração de criptografia de senha
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(
+    plain_password: str,
+    hashed_password: str
+) -> bool:
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
+    )
+
+
+# JWT Token
+
+def create_access_token(
+    data: dict,
+    expires_delta: timedelta | None = None
+):
+
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
